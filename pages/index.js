c8a5a811b6ed48eb4aa1af1710180760c81d0cab
Дмитрий <143331027+Chatbotist@users.noreply.github.com>
2024-08-31 06:49:44 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,12 +32,11 @@ export default function HomePage() {
 
   const handleCopy = () => {
     navigator.clipboard.writeText(hashedText);
-    alert('Скопировано в буфер обмена!');
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
-      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg p-8 w-full max-w-md">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
         <h1 className="text-4xl font-bold mb-4">SHA-256 Кодировщик</h1>
         <form onSubmit={handleSubmit} className="space-y-4 w-full">
           <div>
@@ -49,13 +48,13 @@ export default function HomePage() {
               value={inputText}
               onChange={handleInputChange}
               rows={Math.max(1, inputText.split('\n').length)}
-              className="bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-md px-4 py-2 w-full resize-none"
+              className="bg-gray-100 rounded-md px-4 py-2 w-full resize-none"
             ></textarea>
           </div>
           <div className="flex justify-between">
             <button
               type="submit"
-              className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-md transition-colors duration-300"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md transition-colors duration-300"
             >
               Кодировать
             </button>
@@ -71,14 +70,14 @@ export default function HomePage() {
         {hashedText && (
           <div
             onClick={handleCopy}
-            className="mt-6 bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-md px-4 py-2 cursor-pointer hover:bg-opacity-75 transition-all duration-300 w-full"
+            className="mt-6 bg-gray-100 rounded-md px-4 py-2 cursor-pointer hover:bg-gray-200 transition-all duration-300 w-full"
           >
             <h2 className="text-lg font-medium">Закодированный текст:</h2>
-            <p className="text-xl font-bold">{hashedText}</p>
+            <p className="text-xl font-bold text-blue-500">{hashedText}</p>
           </div>
         )}
         <div className="mt-6 text-center">
-          Разработчик: <a href="https://vk.com/chatbotist" className="text-indigo-300 hover:text-indigo-400">Дмитрий Юрчук</a>
+          Разработчик: <a href="https://vk.com/chatbotist" className="text-blue-500 hover:text-blue-600">Дмитрий Юрчук</a>
         </div>
       </div>
     </div>
